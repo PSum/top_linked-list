8136d3c08294bb4237e54096f4b7e17fbe5a3ff2
psum <sum.philipp@gmail.com>
2023-11-21 21:05:11 +0100

    append not working as intended

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import LinkedList from "./linkedlist.js";
 
 let list1 = new LinkedList(3);
 list1.append(4);
+list1.prepend(2);
+list1.size();
+list1.first();
+list1.tail();
 
-// console.log(list1);
+console.log(list1);
--- src/linkedlist.js
+++ src/linkedlist.js
@@ -4,11 +4,47 @@ export default class LinkedList {
   constructor(data) {
     this.head = new node(data); // Creating the first node
   }
+
   append(value) {
     let current = this.head;
     while (current.next !== null) {
       current = current.next;
     }
     current.next = new node(value);
+  }
+
+  prepend(value) {
+    let current = this.head;
+    let newFirst = new node(value);
+    while (current.next !== null) {
+      newFirst.next = current;
+      current = current.next;
+    }
+    this.head = newFirst;
+  }
+
+  size() {
+    let current = this.head;
+    let size = 1;
+    while (current.next !== null) {
+      size += 1;
+      current = current.next;
+    }
+    console.log(`The size of the linked list is ${size}`);
+  }
+
+  first() {
+    let current = this.head;
+    console.log(`The value of the first node in the list is: ${current.data}`);
+  }
+
+  tail() {
+    let current = this.head;
+    let value = null;
+    while (current.next !== null) {
+      value = current.data;
+      current = current.next;
+    }
+    console.log(`The last value is ${value}`);
   }
 }
