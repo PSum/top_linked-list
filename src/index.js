0e2011dc8098d0a83de8ce561bdac99391460461
PSum <sum.philipp@gmail.com>
2023-11-24 14:14:51 +0100

    finished project with basic functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import node from "./node.js";
 import LinkedList from "./linkedlist.js";
 
 let list1 = new LinkedList(3);
 list1.append(4);
 list1.prepend(2);
+list1.append(8);
+list1.prepend(7);
 list1.size();
 list1.first();
 list1.tail();
 list1.at(1);
+list1.contains(2);
+list1.find(2);
 
 list1.toString();
--- src/linkedlist.js
+++ src/linkedlist.js
@@ -6,22 +6,34 @@ export default class LinkedList {
   }
 
   append(value) {
+    const newNode = new node(value);
+
+    if (!this.head) {
+      // If the list is empty, set the new node as the head
+      this.head = newNode;
+      return;
+    }
+
     let current = this.head;
     while (current.next !== null) {
       current = current.next;
     }
-    current.next = new node(value);
-    this.head = current;
+
+    // Attach the new node at the end of the list
+    current.next = newNode;
   }
 
   prepend(value) {
-    let current = this.head;
-    let newFirst = new node(value);
-    while (current.next !== null) {
-      newFirst.next = current;
-      current = current.next;
+    const newFirst = new node(value);
+
+    if (!this.head) {
+      // If the list is empty, set the new node as the head
+      this.head = newFirst;
+      return;
     }
-    this.head = newFirst;
+
+    newFirst.next = this.head; // Set the new node's next to the current head
+    this.head = newFirst; // Update the head to the new node
   }
 
   size() {
@@ -61,15 +73,61 @@ export default class LinkedList {
   }
 
   pop() {
-    //remove last element
+    if (!this.head) {
+      // Handle the case where the list is empty
+      return;
+    }
+
+    if (!this.head.next) {
+      // Handle the case where there's only one node in the list
+      this.head = null;
+      return;
+    }
+
+    let current = this.head;
+    let previous = null;
+
+    while (current.next !== null) {
+      previous = current;
+      current = current.next;
+    }
+
+    previous.next = null; // Update the pointer of the second-to-last node to null
   }
 
   contains(value) {
     // returns true if the passed in value is in the linked list
+    let current = this.head;
+    try {
+      while (current.data !== null) {
+        if (current.data == value) {
+          console.log(`The value ${value} is part of the linked list`);
+          return true;
+        }
+        current = current.next;
+      }
+    } catch (error) {}
+    console.log(`The value ${value} is not part of the linked list`);
+    return false;
   }
 
   find(value) {
     // return index of passed in value
+    let current = this.head;
+    let i = 0;
+    try {
+      while (current.data !== null) {
+        if (current.data == value) {
+          console.log(`${value} has the index: ${i}`);
+          return;
+        }
+        current = current.next;
+        i += 1;
+      }
+    } catch (error) {}
+    console.log(
+      `The value ${value} is not part of this linked list and therefore has no index`
+    );
   }
 
   toString() {
